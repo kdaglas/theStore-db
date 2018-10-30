d341295f4cbe655118275e538a10ec027a7c783e
Douglas <daglach7@gmail.com>
2018-10-30 04:20:18 +0300

    chore(database) create a database and add the connection
    [Finishes #161525868]

---
+++ storeapp/__init__.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_jwt_extended import JWTManager
+import datetime
+from flask import jsonify
+
+app = Flask(__name__)
+app.config['JWT_SECRET_KEY'] = 'my-secret'
+app.config['JWT_ACCESS_TOKEN EXPIRES'] = datetime.timedelta(days=1)
+jwt = JWTManager(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+
+    ''' this function is for catching an error and 
+        returning a message incase of an invalid url '''
+    return jsonify({"message": "Please put a valid URL"}), 405
+
+
+@app.errorhandler(405)
+def method_not_allowed(e):
+
+    ''' this function is for catching an error and 
+        returning a message incase of a method not allowed '''
+    return jsonify({"message": "Method not allowed"}), 405
+
+
+@app.errorhandler(500)
+def internal_server_error(e):
+
+    ''' this function is for catching an error and 
+        returning a message incase of an internal server error '''
+    return jsonify({"message": "Internal server error"}), 500
+
+# from thestoreapp.views import user_views, product_views, salerecord_views
---
+++ storeapp/database/dbconnector.py
@@ -0,0 +1,92 @@
+import psycopg2
+from storeapp import app
+import psycopg2.extras as dictionary
+
+
+class DatabaseConnection():
+
+    def __init__(self):
+
+        '''This constructor creates a connection to the database depending on the configuration
+            meaning if its a testing environment, then a test database is used where as if its a development
+            environment then a development database is created'''
+        try:
+            if not storeapp.config['TESTING']:
+                self.dbconnection = psycopg2.connect(database="thestoredb", user="postgres",
+                                            password="admin", host="localhost",
+                                            port="5432"
+                                            )
+            else:
+                self.dbconnection = psycopg2.connect(database="testdb", user="postgres",
+                                            password="admin", host="localhost",
+                                            port="5432"
+                                            )
+            self.dbconnection.autocommit = True
+            self.cursor = self.dbconnection.cursor(cursor_factory = dictionary.RealDictCursor)
+        except:
+            print('Cannot connect to the database')
+
+    
+    def create_tables(self):
+
+        '''This function creates the tables'''
+        queries = (
+            """
+            CREATE TABLE IF NOT EXISTS admin (
+                adminId SERIAL PRIMARY KEY NOT NULL,
+                username VARCHAR NOT NULL,
+                password VARCHAR NOT NULL
+            );
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS attendants (
+                attendantId SERIAL PRIMARY KEY NOT NULL,
+                attendant_name VARCHAR NOT NULL,
+                contact VARCHAR NOT NULL,
+                password VARCHAR NOT NULL,
+                role VARCHAR NOT NULL
+            );
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS products (
+                productId SERIAL PRIMARY KEY NOT NULL,
+                product_name VARCHAR NOT NULL,
+                unit_price INTEGER NOT NULL,
+                quantity INTEGER NOT NULL,
+                category VARCHAR NOT NULL
+            )
+            """,
+            """
+            CREATE TABLE IF NOT EXISTS salerecords (
+                saleId SERIAL PRIMARY KEY NOT NULL,
+                attendantId INTEGER NOT NULL,
+                productId INTEGER NOT NULL,
+                quantity INTEGER NOT NULL,
+                pay_amount VARCHAR NOT NULL,
+                today TEXT NOT NULL DEFAULT TO_CHAR(CURRENT_TIMESTAMP, 'HH:MI:SS YYYY-MM-DD'),
+                FOREIGN KEY (attendantId) REFERENCES attendants(attendantId) ON DELETE CASCADE ON UPDATE CASCADE,
+                FOREIGN KEY (productId) REFERENCES products(productId) ON DELETE CASCADE ON UPDATE CASCADE
+            )
+            """
+        )
+        for query in queries:
+            self.cursor.execute(query)
+
+
+    def delete_tables(self):
+
+        '''This function deletes the tables after usage'''
+        delete_queries = (
+            """DROP TABLE IF EXISTS attendants CASCADE""",
+            """DROP TABLE IF EXISTS products CASCADE""",
+            """DROP TABLE IF EXISTS salerecords CASCADE"""
+        )
+        for query in delete_queries:
+            self.cursor.execute(query)     
+
+
+    def closedb(self):
+
+        '''method to close db connection'''
+        self.dbconnection.close()
+        
