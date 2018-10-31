1d2a6318f0c496a09e62247c179e0c20cf42d5f0
Douglas <daglach7@gmail.com>
2018-10-31 17:21:54 +0300

    feat(signup) add the signup feature which is from the views
    This is to add an attendant to the database
    
    [Finishes #161525957]

--- storeapp/views/user_views.py
+++ storeapp/views/user_views.py
@@ -33,3 +33,36 @@ def login():
     access_token = create_access_token(identity=logged_in['attendant_name'], expires_delta=expires)
     return jsonify({"message": "You have been logged in",
                     "token": access_token}), 200
+
+
+@app.route("/api/v2/auth/signup", methods=['POST'])
+# @jwt_required
+def add_attendant():
+
+    ''' Function adds an attendant through POST method by taking in
+        the input from the store owner and storing it in the database '''
+    try:
+        reg_info = request.get_json()
+        attendant_name = reg_info.get("attendant_name")
+        contact = reg_info.get("contact")
+        password = reg_info.get("password")
+        role = "attendant"
+
+        valid = Validator.validate_store_user_credentials(attendant_name, contact, password, role)
+
+        if valid == True:
+            '''Validating and checking for similar data'''
+            same_name = DatabaseQueries().get_attendant_by_name(attendant_name)
+            if same_name:
+                return jsonify({"message":"Attendant name already exists, use another"}), 400
+            same_contact = DatabaseQueries().get_attendant_by_contact(contact)
+            if same_contact:
+                return jsonify({"message":"Contact already exists, use another"}), 400
+            '''Register the customer'''
+            obj = Attendant(attendant_name, contact, password, role)
+            result = obj.add_attendant()
+            return jsonify({"message": result}), 200
+        else:
+            return jsonify({"message":valid}), 400
+    except:
+        return jsonify({"Error": "Some fields are missing, please check"}), 400
