1c8227efd0270e1eef28d6c0b5ae9ac476c08367
Douglas <daglach7@gmail.com>
2018-10-31 18:14:05 +0300

    feat(update) add the endpoint for adding a product
    This is for adding a product to the database
    
    [Finishes #161548131]

---
+++ storeapp/models/product_model.py
@@ -0,0 +1,25 @@
+from storeapp.database.dbconnector import DatabaseConnection
+import psycopg2
+import psycopg2.extras as dictionary
+from storeapp import app
+
+
+dbcon = DatabaseConnection()
+
+'''Object classes for the product model'''
+class Product():
+
+    def __init__(self, product_name, unit_price, quantity, category):
+        '''Declaring all the parameters'''
+        self.product_name = product_name
+        self.unit_price = unit_price
+        self.quantity = quantity
+        self.category = category
+
+
+    def add_product(self):
+        '''method that adds a product to the database'''
+        query = """INSERT INTO products(product_name, unit_price, quantity, category) 
+                    VALUES (%s, %s, %s, %s)"""
+        dbcon.cursor.execute(query, (self.product_name, self.unit_price, self.quantity, self.category))
+        return  "You have successfully added {}".format(self.product_name)
