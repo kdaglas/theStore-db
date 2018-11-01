a729aeb3a2f338f85577b71bfd9738770dcbb39c
Douglas <daglach7@gmail.com>
2018-11-01 16:30:03 +0300

    feat(sale record) add the sale record endpoint

--- storeapp/views/sale_views.py
+++ storeapp/views/sale_views.py
@@ -10,10 +10,10 @@ from flask import request, jsonify, json
 from storeapp.models.sale_model import SaleRecord
 
 product_dbquery = DatabaseQueries()
-sale_dbquery = DatabaseQueries()
+sale_dbquery = SaleDatabaseQueries()
 
-@app.route('/api/v2/sales/<productId>', methods=['POST'])
-def create_sale_record(productId):
+@app.route('/api/v2/sales', methods=['POST'])
+def create_sale_record():
 
     ''' Function for creating a sale record through POST method 
         and storing it in the database '''
@@ -30,76 +30,33 @@ def create_sale_record(productId):
                 return jsonify({"Product selected doesnot exist, choose another"}), 400
             added_sale = sale_dbquery.create_sales_record()
 
-        
-
-        
-            pass
-            '''making checks to the database'''
-            
-            
-
-
-
-            # obj = Product(product_name, unit_price, quantity, category)
-            # result = obj.add_product()
-            # return jsonify({"message": result}), 200
-            pass
         else:
             return jsonify({"message":valid}), 400
     except:
         return jsonify({"Error": "Some fields are missing, please check"}), 400
 
 
-# @app.route('/api/v2/products', methods=['GET'])
-# def fetch_all_products():
+@app.route('/api/v2/sales', methods=['GET'])
+def fetch_all_sale_records():
 
-#     ''' Function that gets all the added products through GET method from the database '''
-#     all_products = dbquery.fetch_all_products()
-#     if not all_products:
-#         return jsonify({"message": "No products added yet"}), 404 
-#     return jsonify({'All_products': all_products,
-#                     'message': 'All products have been viewed'}), 200
-
-
-# @app.route('/api/v2/products/<productId>', methods=['GET'])
-# def fetch_one_product(productId):
-
-#     ''' Function that gets one the added products through GET method from the database '''
-#     valid = Validator.validate_input_type(productId)
-
-#     if valid:
-#         return jsonify({"message":valid}), 400
-#     product = dbquery.fetch_one_product(productId)
-#     if not product:
-#         return jsonify({"message": "No product with that id"}), 404 
-#     return jsonify({'Product': product,
-#                     'message': 'Product has been viewed'}), 200
+    ''' Function that gets all the created sale records through GET method from the database '''
+    all_sale_records = sale_dbquery.fetch_all_sale_records()
+    if not all_sale_records:
+        return jsonify({"message": "No sale recorded created yet"}), 404 
+    return jsonify({'sale_records': all_sale_records,
+                    'message': 'All products have been viewed'}), 200
 
 
-# @app.route('/api/v2/products/<productId>', methods=['DELETE'])
-# def delete_a_product(productId):
+@app.route('/api/v2/sales/<saleId>', methods=['GET'])
+def fetch_one_sale(saleId):
 
-#     ''' Function that deletes an added product through DELETE method from the database '''
-#     valid = Validator.validate_input_type(productId)
+    ''' Function that gets one created sale record through GET method from the database '''
+    valid = Validator.validate_input_type(saleId)
 
-#     if valid:
-#         return jsonify({"message":valid}), 400
-#     deleted = dbquery.delete_one_product(productId)
-#     if not deleted:
-#         return jsonify({"message": "No products with that id"}), 404 
-#     return jsonify({'message': "Successfully deleted product"}), 200
-
-
-# @app.route('/api/v2/products/<productId>', methods=['PUT'])
-# def update_a_product(productId):
-
-#     '''Function for updating the quantity or price of a product '''
-#     pdt_info = request.get_json()
-#     unit_price = pdt_info.get("unit_price")
-#     quantity = pdt_info.get("quantity")
-
-#     updated = dbquery.update_one_product(productId, unit_price, quantity)
-#     if updated:
-#         print('success')
-#         return jsonify({'message': 'Product has been updated'}), 200
-#     return jsonify({'message': 'Product could not be updated'}), 400
+    if valid:
+        return jsonify({"message":valid}), 400
+    sale_record = sale_dbquery.fetch_one_sale_record(saleId)
+    if not sale_record:
+        return jsonify({"message": "No product with that id"}), 404 
+    return jsonify({'sale_record': sale_record,
+                    'message': 'Sale record has been viewed'}), 200
