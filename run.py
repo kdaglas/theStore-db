459bfcb301488cb4e32cd013d6fdc494bd11a0d6
Douglas <daglach7@gmail.com>
2018-11-14 19:17:24 -0800

    feat(app) add function that can create the admin in the db

--- run.py
+++ run.py
@@ -1,8 +1,12 @@
 from storeapp import app
 from storeapp.database.dbconnector import DatabaseConnection
+from storeapp.database.dbuserqueries import UserDatabaseQueries
 
+
+dbquery = UserDatabaseQueries()
 
 if __name__== "__main__":
     DatabaseConnection().create_tables()
+    dbquery.add_admin()
     app.run(debug=True)
     
