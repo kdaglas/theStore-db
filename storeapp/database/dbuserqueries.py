2bd24b1bf71459ed7ff022c60e79e24bdffefbaf
Douglas <daglach7@gmail.com>
2018-11-14 19:14:58 -0800

    chore(database) modify the queries to accomodate more user functions

--- storeapp/database/dbuserqueries.py
+++ storeapp/database/dbuserqueries.py
@@ -1,3 +1,4 @@
+'''package imports '''
 from storeapp.database.dbconnector import DatabaseConnection
 from storeapp.models.user_model import Attendant
 import psycopg2
@@ -8,9 +9,16 @@ dbcon = DatabaseConnection()
 
 class UserDatabaseQueries():
 
-    '''these are methods to perform certain queries to the database'''
+    '''performing certain queries to the database'''
+    def add_admin(self):
+        '''registers an admin'''
+        query = """INSERT INTO attendants(attendant_name, contact, password, role) 
+                    VALUES (%s, %s, %s, %s)"""      
+        dbcon.cursor.execute(query, ('admin', '+256-700-000000', 'Admin17', 'admin'))
+
+
     def authenticate_attendant(self, attendant_name, password):
-        '''method for logging in an attendant'''
+        '''logging in a user'''
         query = """SELECT attendant_name, password FROM attendants WHERE attendant_name = %s and password = %s"""
         dbcon.cursor.execute(query, (attendant_name, password,))
         login = dbcon.cursor.fetchone()
@@ -18,38 +26,56 @@ class UserDatabaseQueries():
 
 
     def fetch_all_attendants(self):
-        '''method that retieves all the attendants from the database'''
+        '''retieves all the attendants'''
         query = """SELECT * FROM attendants"""
         dbcon.cursor.execute(query)
         attendants = dbcon.cursor.fetchall()
         return attendants
 
 
+    def fetch_one_attendant(self, attendantId):
+        '''gets one attendant'''
+        query = """SELECT * FROM attendants WHERE attendantId = %s"""
+        dbcon.cursor.execute(query, (attendantId,))
+        attendant = dbcon.cursor.fetchone()
+        return attendant
+
+
     def get_attendant_by_name(self, attendant_name):
-        '''method to return an attendant by name the db'''
+        '''return an attendant by name'''
         query = """SELECT * FROM attendants WHERE attendant_name = %s"""
         dbcon.cursor.execute(query, (attendant_name,))
         attendant = dbcon.cursor.fetchone()
         return attendant
 
 
-    def get_attendant_by_role(self, role):
-        '''method to return an attendant by role the db'''
-        query = """SELECT * FROM attendants WHERE role = %s"""
-        dbcon.cursor.execute(query, (role,))
-        attendant = dbcon.cursor.fetchone()
-        return attendant
-
-
     def get_attendant_by_contact(self, contact):
-        '''method that checks if attendant contact is already added in the database'''
+        '''checks if attendant contact already exists'''
         query = """SELECT * FROM attendants WHERE contact = %s"""
         dbcon.cursor.execute(query, (contact,))
         attendant = dbcon.cursor.fetchone()
         return attendant
 
 
+    def get_attendant_by_role(self, role):
+        '''bring back an attendant by role'''
+        query = """SELECT * FROM attendants WHERE role = %s"""
+        dbcon.cursor.execute(query, (role,))
+        attendant = dbcon.cursor.fetchone()
+        return attendant
+
+
     def promote_to_admin(self, role, attendantId):
-        '''this method returns the new admin'''
+        '''updating role of a user'''
         query = """UPDATE attendants SET role = %s WHERE attendantId = %s"""
         dbcon.cursor.execute(query, (attendantId, role,))
+        promoted = dbcon.cursor.rowcount
+        return promoted
+
+
+    def delete_user(self, attendantId):
+        '''deletes attendant from the database'''
+        query = """DELETE FROM attendants WHERE attendantId = %s"""
+        dbcon.cursor.execute(query, (attendantId,))
+        deleted_attendant = dbcon.cursor.rowcount
+        return deleted_attendant
