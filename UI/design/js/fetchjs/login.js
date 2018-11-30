009ededdc1a47246b255f86ce07ca53118fa7137
Douglas <daglach7@gmail.com>
2018-11-29 18:39:42 -0800

    feat(app) add all required files with modification for the login js

--- UI/design/js/fetchjs/login.js
+++ UI/design/js/fetchjs/login.js
@@ -1,46 +1,45 @@
-try{
-    document.getElementById('log').addEventListener('submit', login);
+document.getElementById('logging-in').addEventListener('submit', login);
 
-    function login(e){
+function login(e){
 
-        e.preventDefault();
-        let username = document.getElementById('username').value;
-        let password = document.getElementById('password').value;
+    e.preventDefault();
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
 
-        fetch('http://127.0.0.1:5000/api/v2/auth/login', {
-            method: 'POST',
-            headers: {
-                'Accept': 'application/json',
-                'Content-type': 'application/json'
-            },
-            body:JSON.stringify({attendant_name:username, password:password})
-        })
-        .then((response) => {
-            status_code = response.status
-            return response.json()
-        })
-        .then((data) => {
-            var err = document.getElementById('errormsg')
-            if(status_code == 401){
-                err.innerHTML = data['message']
-            }
-            else if(status_code == 200 && data["role"] == "admin"){
-                var token= data['token']
-                sessionStorage.setItem('token', token)
-                window.location.href = 'admin.html'
-            }
-            else if (status_code == 200 && data["role"] == "attendant"){
-                var token= data['token']
-                sessionStorage.setItem('token', token)
-                window.location.href = 'homepage.html'
-            }
-            else{
-                err.innerHTML = data['message']
-            }
-        })
-        .catch((error) => {
-            console.log(error)
-        })
-    }
+    fetch('http://127.0.0.1:5000/api/v2/auth/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-type': 'application/json'
+        },
+        body:JSON.stringify({attendant_name:username, password:password})
+    })
+    .then((response) => {
+        status_code = response.status
+        return response.json()
+    })
+    .then((data) => {
+        var err = document.getElementById('errormsg')
+        var token = data['token']
+        var role = data['role']
+        if(status_code == 401){
+            err.innerHTML = data['message']
+        }
+        else if(status_code == 200 && data["role"] == "admin"){
+            sessionStorage.setItem('token', token)
+            sessionStorage.setItem('role', role)
+            window.location.href = 'admin.html'
+        }
+        else if(status_code == 200 && data["role"] == "attendant"){
+            sessionStorage.setItem('token', token)
+            sessionStorage.setItem('role', role)
+            window.location.href = 'homepage.html'
+        }
+        else{
+            err.innerHTML = data['message']
+        }
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 }
-catch(error){}
